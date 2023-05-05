fd6eb76d1d4642e26409bc7664141f94b3e18060
Domae-back-end <ty_ty123@naver.com>
2023-05-05 11:19:13 +0900

    create: spring 프로젝트 생성

---
+++ src/test/java/com/server/minecraftserver/MinecraftServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.minecraftserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinecraftServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
